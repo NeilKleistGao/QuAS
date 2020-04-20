973e5612c769f75aec215ae1eba9fd114617e8b3
NeilKleistGao <neilkleistgao@163.com>
2020-04-20 22:08:05 +0800

    init

---
+++ backend/src/main/java/com/quas/questionnaire/QuasApplication.java
@@ -0,0 +1,13 @@
+package com.quas.questionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/quas/questionnaire/QuasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quas.questionnaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
